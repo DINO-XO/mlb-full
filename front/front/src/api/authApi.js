de0072a570299d01f85611814a2b565b0712365c
DINO-XO <130955287+DINO-XO@users.noreply.github.com>
2026-01-08 10:11:26 +0530

    Update authApi.js

--- front/front/src/api/authApi.js
+++ front/front/src/api/authApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/api/auth";
+const API_URL = "https://mlb-lab.onrender.com/api/auth";
 
 // ✅ LOGIN
 export const loginUser = (data) => {
